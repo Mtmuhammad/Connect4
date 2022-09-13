1cccb6f3e536d4fb78562320c189987d0b1e8062
mtmuhammad <marcellus.muhammad@yahoo.com>
2022-09-13 12:05:58 -0400

    Initial Commit
    Initial commit for connect 4 JS game.

---
+++ connect4.js
@@ -0,0 +1,6 @@
+/** Connect Four
+ *
+ * Player 1 and 2 alternate turns. On each turn, a piece is dropped down a
+ * column until a player gets four-in-a-row (horiz, vert, or diag) or until
+ * board fills (tie)
+ */
